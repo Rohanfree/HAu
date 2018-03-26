aea25d039eeec0987ce5aac00f46b8e41401115c
sharun thomas <33017354+thomassharun@users.noreply.github.com>
2018-03-27 01:38:24 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/rohan/hau/user.java
@@ -0,0 +1,10 @@
+package com.example.rohan.hau;
+
+public class user {
+    public String address;
+    public String cno;
+    public String mobile;
+    public String name;
+    public String piname;
+    //public int value;
+}
